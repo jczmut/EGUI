af4ad03925e1f92f1f6c010d14b5fc9000a692c8
jczmut <56881021+jczmut@users.noreply.github.com>
2020-05-31 12:48:00 +0200

    Delete MainWrapper.js

--- project 3/src/MainWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MainWrapper(props) {
-    return (
-        <div id="mainwrapper" className={props.smallversion ? "small-version" : ""}>
-            {props.children}  
-        </div>
-    )
-}
